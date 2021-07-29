a47e5d3e8533293d3baa58d170705366aec045b5
Cory Bell <corybell21@gmail.com>
2021-07-28 20:55:11 -0500

    update relay service with new arduino protocol

--- services/relay.py
+++ services/relay.py
@@ -5,26 +5,15 @@ class RelayService():
   def __init__(self, serial_service: SerialComService):
     self.serial_service = serial_service
 
-  def is_relay_on(self, relay_id: str) -> bool:
-    return self.relay_status(relay_id) == serial_response.RELAY_ON
-
   def relay_on(self, relay_id: str):
-    if not self.is_relay_on(relay_id):
-      return self.toggle_relay(relay_id)
-    return serial_response.SUCCESS
+    command = serial_command.RELAY_ON.get(relay_id)
+    if not command:
+      return serial_response.BAD_REQUEST
+    return self.serial_service.request(command)
 
   def relay_off(self, relay_id: str):
-    if self.is_relay_on(relay_id):
-      return self.toggle_relay(relay_id)
-    return serial_response.SUCCESS
-
-  def toggle_relay(self, relay_id: str):
-    c = serial_command.RELAY_TOGGLE[relay_id]
-    resp = self.serial_service.request(c)
-    return resp
-
-  def relay_status(self, relay_id: str):
-    c = serial_command.RELAY_STATUS[relay_id]
-    resp = self.serial_service.request(c)
-    return resp
+    command = serial_command.RELAY_OFF.get(relay_id)
+    if not command:
+      return serial_response.BAD_REQUEST;
+    return self.serial_service.request(command)
   
